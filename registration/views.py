fee559f24d1235882404b9c383b34c8b5da3e204
slizer98 <erick.m.villa.r@gmail.com>
2024-05-27 12:34:13 -0600

    register form created

--- registration/views.py
+++ registration/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from .forms import UserCreationFormsWithEmail
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django import forms
 
 # Create your views here.
+class SingUpView(CreateView):
+  form_class = UserCreationFormsWithEmail
+  success_url = reverse_lazy('login')
+  template_name = 'registration/signup.html'
+  
+  def get_success_url(self) -> str:
+    return reverse_lazy('login') + '?register'
+  
+  def get_form(self, form_class=None):
+    form = super(SingUpView, self).get_form()
+    form.fields['username'].widget = forms.TextInput(attrs={'class': 'form-control mb-2', 'placeholder': 'Nombre de usuario'})
+    form.fields['email'].widget = forms.EmailInput(attrs={'class': 'form-control mb-2', 'placeholder': 'Correo Electronico'})
+    form.fields['password1'].widget = forms.PasswordInput(attrs={'class': 'form-control mb-2', 'placeholder': 'Contraseña'})
+    form.fields['password2'].widget = forms.PasswordInput(attrs={'class': 'form-control mb-2', 'placeholder': 'Repite tu Contraseña'})
+    return form
+  
