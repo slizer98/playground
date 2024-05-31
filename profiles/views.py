f1da676fd22f2d0ceec54d28216b2b01c8433aa9
slizer98 <erick.m.villa.r@gmail.com>
2024-05-31 07:43:33 -0600

    pagination added

--- profiles/views.py
+++ profiles/views.py
@@ -7,6 +7,7 @@ from registration.models import Profile
 class ProfileListView(ListView):
   model = Profile
   template_name = 'profiles/profile_list.html'
+  paginate_by = 3
   
 class ProfileDetail(DetailView):
   model = Profile  
--- registration/models.py
+++ registration/models.py
@@ -14,6 +14,9 @@ class Profile(models.Model):
   avatar = models.ImageField(upload_to=custom_upload_to, null=True, blank=True)
   bio = models.TextField(null=True, blank=True)
   link = models.URLField(max_length=200, null=True, blank=True)
+  
+  class Meta:
+    ordering = ['user__username']
 
 
 @receiver(post_save, sender=User)
