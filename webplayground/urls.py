f3a3d61ff025fb2864b3219d9bd679ad91384a40
slizer98 <erick.m.villa.r@gmail.com>
2024-06-01 10:38:22 -0600

    message implementation, finished

--- webplayground/urls.py
+++ webplayground/urls.py
@@ -3,12 +3,14 @@ from django.contrib import admin
 from django.urls import path, include
 from pages.urls import pages_patterns
 from profiles.urls import profiles_patterns
+from messeger.urls import messeger_patterns
 from django.conf import settings
 
 urlpatterns = [
     path('', include('core.urls')),
     path('pages/', include(pages_patterns)),
     path('profiles/', include(profiles_patterns)),
+    path('messenger/', include(messeger_patterns)),
     path('admin/', admin.site.urls),
     # auths paths
     path('accounts/', include('django.contrib.auth.urls')),
